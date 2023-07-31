ada82c3172a1c824569e4c1d0e66eab54c0ddb31
Pankaj Batham <51159344+Pankajbatham27@users.noreply.github.com>
2023-07-31 15:04:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import CardBorder from "./Components/CardBorder/CardBorder";
+
+function App() {
+
+  return (
+    <div className="app">
+      <div style={{ height: '200px', width: '200px', background: '#000', position: 'relative', margin: '10px' }}>
+        <CardBorder />
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
